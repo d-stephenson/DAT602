c06d96f6f5ad5e4c258016d9d86f7ce50531c5fb
Dale Stephenson <dalestephenson@zohomail.com>
2021-04-07 20:09:10 +1200

    Login check credentials procedure

--- DAT602_MS2_game.sql
+++ DAT602_MS2_game.sql
@@ -5,3 +5,36 @@
 ----------------------------------------------------------------------------------
 
 USE sdghGameDatabase;
+
+----------------------------------------------------------------------------------
+-- Login Check Credentials Procedures
+----------------------------------------------------------------------------------
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS loginCheckCredentials;
+CREATE PROCEDURE loginCheckCredentials(IN pUsername varchar(50), IN pPassword varchar(15))
+BEGIN
+    DECLARE proposedUID int DEFAULT NULL;
+  
+	SELECT PlayerID 
+	FROM 
+		tblPlayer
+	WHERE
+		Username = pUsername AND 
+		`Password` = pPassword
+	INTO proposedUID;
+     
+    IF proposedUID IS NULL THEN
+		UPDATE tblPlayer
+        SET FailedLogins = FailedLogins +1, AccountLocked = (FailedLogins +1) > 5, ActiveStatus = (FailedLogins +1) < 1
+        WHERE Username = pUsername; 
+	ELSEIF proposedUID IS NOT NULL THEN
+		UPDATE tblPlayer
+        SET ActiveStatus = 1, FailedLogins = 0, AccountLocked = 0
+        WHERE Username = pUsername; 
+	END IF;
+     
+END //
+DELIMITER ;
+
+CALL loginCheckCredentials('John',1234);
