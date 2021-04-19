83418ab27df3b9e1d17d588df9e44df06209ae8a
Dale Stephenson <dalestephenson@zohomail.com>
2021-04-20 11:30:02 +1200

    now selecting only remaining characters in game

--- DAT602_MS2_game.sql
+++ DAT602_MS2_game.sql
@@ -176,15 +176,15 @@ BEGIN
 	IF GameID = pGameID THEN
 		INSERT INTO tblPlay(PlayerID, CharacterName, GameID)
 		VALUES ((SELECT PlayerID FROM tblPlayer WHERE Username = pUsername),
-                (SELECT ch.CharacterName, pl.CharacterName, pl.Game, ga.GameID
-                 FROM tblCharacter ch 
-                    JOIN tblPlay pl ON ch.CharacterName = pl.CharacterName
-                    JOIN tblGame ga ON pl.GameID = ga.GameID
-                 WHERE NOT EXISTS
-                    (SELECT NULL FROM GameID = 100002;), 
+                (SELECT CharacterName
+				 FROM tblCharacter 
+				 WHERE CharacterName NOT IN (SELECT CharacterName FROM tblPlay WHERE GameID = pGamieID);  
                  pGameID);
     END IF;  
 END //
 DELIMITER ;
 
 CALL joinGame(100002, 'Sunny');
+
+
+      
