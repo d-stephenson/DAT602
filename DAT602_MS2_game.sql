d0681dd99f00d192b726ea3bb3bd5ce3d11b6e4f
Dale Stephenson <dalestephenson@zohomail.com>
2021-05-03 09:38:03 +1200

    Need to update to allow player to move to a tile if another person is on tile but their active status is 0

--- DAT602_MS2_game.sql
+++ DAT602_MS2_game.sql
@@ -392,7 +392,7 @@ BEGIN
 		(newTileColumn = currentTileColumn OR newTileColumn = currentTileColumn + 1 OR newTileColumn = currentTileColumn - 1)) AND
         (emptyTile IS NOT NULL OR pTileID = 001) AND
         (currentTurn = (SELECT CharacterName FROM tblPlay WHERE PlayerID = pPlayerID AND GameID = pGameID)) THEN  
-        
+        -- Add if player active status = 0 then can move to that tile
 		UPDATE tblPlay
 		SET TileID = pTileID
 		WHERE PlayerID = pPlayerID AND GameID = pGameID;
