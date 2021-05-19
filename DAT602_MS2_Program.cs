e5480d343390dd0e54ddd1864a20721637a0d581
Dale <dalestephenson@protonmail.com>
2021-05-19 20:12:54 +1200

    Program.cs code corrections

--- DAT602_MS2_Program.cs
+++ DAT602_MS2_Program.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace DAT602_ConsoleApp
 {
