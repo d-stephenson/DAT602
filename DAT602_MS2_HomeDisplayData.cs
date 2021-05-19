60313443818fe50cc84efefa4fce974d87a4940c
Dale <dalestephenson@protonmail.com>
2021-05-19 19:47:53 +1200

    Create GameCount, PlayerHighScore, HomeDisplayData

---
+++ DAT602_MS2_HomeDisplayData.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DAT602_ConsoleApp
+{
+    class HomeDisplayData
+    {
+        public List<GameCount> GameCount;
+        public List<PlayerHighScore> PlayerHighScore;
+    }
+}
---
+++ DAT602_MS2_PlayerHighScore.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DAT602_ConsoleApp
+{
+    class PlayerHighScore
+    {
+        public string Player;
+        public int HighScore;
+
+        // 		SELECT Username AS 'Player', HighScore AS 'High Score' 
+    }
+}
