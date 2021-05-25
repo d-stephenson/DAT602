76801fafe20fa2f92405859d851db675562f017b
Dale <dalestephenson@protonmail.com>
2021-05-25 18:39:06 +1200

    Add classes for tile display data and tile info

---
+++ DAT602_MS2_TileInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAT602_ConsoleApp
+{
+    class TileInfo
+    {
+        public string TileColour;
+        public int TileRow;
+        public int TileColumn;
+
+
+        // TileColour, TileRow, TileColumn
+    }
+}
