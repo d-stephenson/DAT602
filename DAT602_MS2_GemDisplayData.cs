5136d781d39c2abc0d08b621a90f6ee82a8373bd
Dale <dalestephenson@protonmail.com>
2021-05-25 18:19:11 +1200

    add classes: gemselection and gem display data

---
+++ DAT602_MS2_GemDisplayData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAT602_ConsoleApp
+{
+    class GemDisplayData
+    {
+        public string message;
+        public bool haveGem;
+        public List<GemSelection> GemSelection;
+    }
+}
---
+++ DAT602_MS2_GemSelection.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAT602_ConsoleApp
+{
+    class GemSelection
+    {
+        public int ItemID;
+        public string GemType;
+        public int Points;
+        public int GameID;
+        public int PlayerID;
+        public int PlayID;
+        public int TileID;
+
+
+        // 		ItemID, ge.GemType, Points, pl.GameID, pl.PlayerID, pl.PlayID, pl.TileID
+    }
+}
